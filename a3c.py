580a24c381e49cdde8b956aab750066efaff50ae
4SkyNet <nemiroff.den@gmail.com>
2017-09-19 00:53:01 +0300

    upd: adjust hyperparams for maze

--- a3c.py
+++ a3c.py
@@ -223,8 +223,10 @@ should be computed.
             # on the one hand;  but on the other hand, we get less frequent parameter updates, which
             # slows down learning.  In this code, we found that making local steps be much
             # smaller than 20 makes the algorithm more difficult to tune and to get to work.
-            self.runner = RunnerThread(env, pi, 20, visualise)
-
+            local_steps = 20
+            if maze:
+                local_steps = 10
+            self.runner = RunnerThread(env, pi, local_steps, visualise)
 
             grads = tf.gradients(self.loss, pi.var_list)
 
@@ -255,7 +257,10 @@ should be computed.
             inc_step = self.global_step.assign_add(tf.shape(pi.x)[0])
 
             # each worker has a different set of adam optimizer parameters
-            opt = tf.train.AdamOptimizer(1e-4)
+            lr = 1e-4
+            if maze:
+                lr = 1e-3
+            opt = tf.train.AdamOptimizer(lr)
             self.train_op = tf.group(opt.apply_gradients(grads_and_vars), inc_step)
             self.summary_writer = None
             self.local_steps = 0
